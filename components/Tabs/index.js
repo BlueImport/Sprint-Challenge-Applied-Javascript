7c40115fef2e14a06e4b47d4c7ffb55a662ebcfc
Kris <kristian.claire.101@gmail.com>
2019-10-04 15:06:38 -0700

    adds 5 tabs to topics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -11,15 +11,27 @@
 
 //get the axios
 axios
-.get("https://lambda-times-backend.herokuapp.com/topics")
-.then(function(response) {
-  // Success!
-  console.log(response);
+//get the axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    console.log(response);
+    response.data.articles.javascript.forEach(article => {
+        cardsContainer.appendChild(techCards(article))
+    });
+    response.data.articles.bootstrap.forEach(article => {
+        cardsContainer.appendChild(techCards(article))
+    });
+    response.data.articles.technology.forEach(article => {
+        cardsContainer.appendChild(techCards(article))
+    });
+    response.data.articles.jquery.forEach(article => {
+        cardsContainer.appendChild(techCards(article))
+    });
+    response.data.articles.node.forEach(article => {
+        cardsContainer.appendChild(techCards(article))
+    });
 })
-.catch(function(error) {
-  // Error!
-  console.log(error);
-});
+
 
 //set main query selector
 const tabs = document.querySelector('.tabs');
@@ -29,17 +41,38 @@ function allTabs(data) {
 
 //creates elements
 const topics = document.createElement('topics');
-const tab = document.createElement('div');
+// const tab = document.createElement('div');
+const tab1 = document.createElement('div');
+const tab2 = document.createElement('div');
+const tab3 = document.createElement('div');
+const tab4 = document.createElement('div');
+const tab5 = document.createElement('div');
 
 //adds classes
 topics.classList.add('topics');
-tab.classList.add('tab');
+// tab.classList.add('tab');
+tab1.classList.add('tab');
+tab2.classList.add('tab');
+tab3.classList.add('tab');
+tab4.classList.add('tab');
+tab5.classList.add('tab');
+
 
 //adds text content
-tab.textContent = 'topic here';
+// tab.textContent = 'topic here';
+tab1.textContent = 'Javascript';
+tab2.textContent = 'Bootstrap';
+tab3.textContent = 'Technology';
+tab4.textContent = 'JQuery';
+tab5.textContent = 'Node';
 
 //appends the two together
-topics.appendChild(tab);
+// topics.appendChild(tab);
+topics.appendChild(tab1);
+topics.appendChild(tab2);
+topics.appendChild(tab3);
+topics.appendChild(tab4);
+topics.appendChild(tab5);
 
 console.log(topics);
 return topics;
